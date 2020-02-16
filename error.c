368413569d812eaa9e05093372369e6432d579b7
ohtou <fukujinn@gmail.com>
2020-02-17 02:27:36 +0900

    add error_at()

--- error.c
+++ error.c
@@ -15,3 +15,17 @@ void error_str(char *fmt, ...) {
   fprintf(stderr, " : errno=%d, %s\n", errno, strerror(errno));
   exit(1);
 }
+
+void error_at(char *user_input, char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
