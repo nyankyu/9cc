b307031b8bf3da82be3d6f5412619c73b022afdb
ohtou <fukujinn@gmail.com>
2020-02-18 23:47:04 +0900

    make AST header file

---
+++ ast.h
@@ -0,0 +1,19 @@
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+
